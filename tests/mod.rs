cb935495588b52e96371f908d058b1160477cee6
Luni-4 <luni-4@hotmail.it>
2023-12-07 16:17:42 +0100

    wip

--- tests/mod.rs
+++ tests/mod.rs
@@ -7,7 +7,7 @@ use walkdir::WalkDir;
 use complex_code_spotter::{Complexity, OutputFormat, Snippets, SnippetsProducer};
 
 const SOURCE_PATH: &str = "data/seahorse/src";
-const SNAPSHOT_PATH: &str = "tests/snapshots";
+const SNAPSHOT_PATH: &str = "snapshots";
 const TMP_DIR: &str = "complex-code-spotter";
 
 #[inline(always)]
@@ -21,7 +21,7 @@ fn run_tests(subdir: &str, complexities: Vec<(Complexity, usize)>) {
     // Snapshot path
     let snapshot_path = Path::new(SNAPSHOT_PATH).join(subdir);
 
-    // Create low values directory
+    // Create subdirectories
     create_dir_all(&snapshot_path).unwrap();
 
     // Temporary path
@@ -33,6 +33,7 @@ fn run_tests(subdir: &str, complexities: Vec<(Complexity, usize)>) {
     // Produce snippets
     SnippetsProducer::new()
         .complexities(complexities)
+        .enable_write()
         .output_format(OutputFormat::Json)
         .run(Path::new(SOURCE_PATH), &tmp_path)
         .unwrap();
@@ -42,33 +43,35 @@ fn run_tests(subdir: &str, complexities: Vec<(Complexity, usize)>) {
         let entry = entry.unwrap();
         let entry = entry.path();
 
-        // Read file
-        let snippet = read_snippets(entry);
+        if entry.is_file() {
+            // Read file
+            let snippet = read_snippets(entry);
 
-        // Snapshot name
-        let name = entry.file_name().and_then(|v| v.to_str()).unwrap();
+            // Snapshot name
+            let name = entry.file_name().and_then(|v| v.to_str()).unwrap();
 
-        insta::with_settings!({
-            snapshot_path => snapshot_path.join(entry),
-            prepend_module_to_snapshot => false
-        },{
-            insta::assert_json_snapshot!(name, snippet);
-        });
+            insta::with_settings!({
+                snapshot_path => &snapshot_path,
+                prepend_module_to_snapshot => false
+            },{
+                insta::assert_yaml_snapshot!(name, snippet);
+            });
+        }
     }
 }
 
 #[test]
 fn seahorse_high_thresholds() {
     run_tests(
-        "high_values",
+        "high_thresholds",
         vec![(Complexity::Cyclomatic, 15), (Complexity::Cognitive, 15)],
     );
 }
 
-#[test]
+/*#[test]
 fn seahorse_low_thresholds() {
     run_tests(
-        "low_values",
+        "low_thresholds",
         vec![(Complexity::Cyclomatic, 1), (Complexity::Cognitive, 1)],
     );
-}
+}*/
