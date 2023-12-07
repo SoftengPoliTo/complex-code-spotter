bbeea24500f53e87ae023e89f4e388be51212743
Luni-4 <luni-4@hotmail.it>
2023-12-07 10:59:34 +0100

    wip

--- tests/mod.rs
+++ tests/mod.rs
@@ -1,23 +1,46 @@
+use std::fs::create_dir_all;
 use std::path::Path;
 
 use walkdir::WalkDir;
 
-const SNAPSHOT_PATH: &str = "../snapshots/low_values";
+use complex_code_spotter::{Complexity, OutputFormat, SnippetsProducer};
+
+const SOURCE_PATH: &str = "data/seahorse/src";
+const SNAPSHOT_PATH: &str = "tests/snapshots";
+const TMP_DIR: &str = "complex-code-spotter";
 
 #[test]
 fn test_low_values() {
-    // Create temporary results
+    // Snapshot path
+    let snapshot_path = Path::new(SNAPSHOT_PATH).join("low_values");
+
+    // Create low values directory
+    create_dir_all(&snapshot_path).unwrap();
+
+    // Temporary path
+    let tmp_path = std::env::temp_dir().join(TMP_DIR);
+
+    // Create directory in tmp directory
+    create_dir_all(&tmp_path).unwrap();
+
+    // Produce snippets
+    SnippetsProducer::new()
+        .complexities(vec![
+            (Complexity::Cyclomatic, 1),
+            (Complexity::Cognitive, 1),
+        ])
+        .output_format(OutputFormat::Json)
+        .run(Path::new(SOURCE_PATH), tmp_path)
+        .unwrap();
 
     // Iterate over temporary results
-    for entry in WalkDir::new(path)
-        .follow_links(true)
-        .into_iter()
-        .filter_map(|e| e.ok())
-    {
+    for entry in WalkDir::new(tmp_path.join(OutputFormat::Json.into())).into_iter() {
+        let entry = entry.unwrap();
+
         // Read snapshot
 
         insta::with_settings!({
-            snapshot_path => Path::new(snapshot_path).join(entry),
+            snapshot_path => snapshot_path.join(entry.path()),
             prepend_module_to_snapshot => false
         },{
             insta::assert_json_snapshot!(name, content);
